d57197d31ad94f2b80ec636170ff3eb852355370
Elezabeth <elezabethmatthew@gmail.com>
2020-01-27 22:05:55 +0100

    Committed successfully

---
+++ CountDown/src/CountDownAndDivision.java
@@ -0,0 +1,25 @@
+
+public class CountDownAndDivision {
+
+	
+		public static void main(String[] args) {
+		    for(int i=100;i>=1;i--) {
+		    	if(i%5==0&&i%3==0) {
+		    		System.out.println("Testing");
+		    	}
+		    	else if(i%3==0) {
+		    		System.out.println("Software");
+		    	}
+		    	else if(i%5==0) {
+		    		System.out.println("Agile");
+		    	
+		    	}
+		    	else {
+		    		System.out.println("The number is "+i);
+		    		
+		    	}
+		    }
+			}
+	}
+
+
